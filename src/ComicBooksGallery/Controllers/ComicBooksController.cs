a767dfc96bfddfea59582629c2e2b0bbbb358305
Ernesto Emmanuel Gutierrez Muñoz <netoxgtz@outlook.com>
2017-12-04 23:21:49 -0600

    Added View to ComicBooksController

--- src/ComicBooksGallery/Controllers/ComicBooksController.cs
+++ src/ComicBooksGallery/Controllers/ComicBooksController.cs
@@ -8,12 +8,32 @@ namespace ComicBooksGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        /*public string Detail()
         {
             return "message from the controller";
+        }*/
+
+        /*public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult() { Content = "message from the controller" };
+        }*/
+
+        /*public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+
+                return Redirect("/");
+            
+            return Content("message from the controller");
+        }*/
+
+        public ActionResult Detail()
+        {
+            return View();
         }
-
-
-
     }
 }
